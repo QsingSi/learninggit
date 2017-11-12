6ea3412724419599182801daa3f45f2c67b0b79e
weizq <wzq67675@163.com>
2017-11-12 23:05:18 +0800

    graphic

--- tinker/tinkerTest.py
+++ tinker/tinkerTest.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.messagebox as messagebox
+import socket
 
 
 class Appliacation(Frame):
@@ -7,15 +8,37 @@ class Appliacation(Frame):
         Frame.__init__(self, master)
         self.pack()
         self.creatWindow()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__ip = '127.0.0.1'
+        self.flag = False
+        self.__MAXLEN = 1024
 
     def creatWindow(self):
+        if self.flag == True:
+            messagebox.showinfo(
+                'Message', 'initialize succes! start listening:')
         self.nameInput = Entry(self)
         self.nameInput.pack()
         self.button = Button(self, text='Hello', command=self.hello)
         self.button.pack()
+
     def hello(self):
         name = self.nameInput.get() or 'world'
-        messagebox.showinfo('Message', 'hello,{}'.format(name))
+        data, addr = self.recieve()
+        #messagebox.showinfo('Message', 'hello,{}'.format(name))
+        self.s.sendto(b'hello,%s' % data, addr)
+        info = 'send {} to address: {}'.format(name, addr)
+        self.nameOutput = Label(self, text=info)
+        self.nameOutput.pack()
+
+    def set_param(self, port=9999):
+        param = (self.__ip, port)
+        self.s.bind(param)
+        self.flag = True
+
+    def recieve(self):
+        data, addr = self.s.recvfrom(self.__MAXLEN)
+        return data, addr
 
 
 app = Appliacation()
