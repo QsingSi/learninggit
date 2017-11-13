9c3b345c67c851e8032de2dc54d37a9e3e71fc47
weizq <wzq67675@163.com>
2017-11-13 23:37:08 +0800

    light --version

--- tinker/tinkerTest.py
+++ tinker/tinkerTest.py
@@ -10,31 +10,35 @@ class Appliacation(Frame):
         self.creatWindow()
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__ip = '127.0.0.1'
-        self.flag = False
         self.__MAXLEN = 1024
 
     def creatWindow(self):
-        if self.flag == True:
-            messagebox.showinfo(
-                'Message', 'initialize succes! start listening:')
+        messagebox.showinfo(
+            'Message', 'initialize succes! start listening:')
+        self.listen = Label(self, text='start listen the port {}'.format(9999))
+        self.listen.pack()
         self.nameInput = Entry(self)
         self.nameInput.pack()
-        self.button = Button(self, text='Hello', command=self.hello)
+        self.button = Button(self, text='Send', command=self.hello)
         self.button.pack()
 
     def hello(self):
         name = self.nameInput.get() or 'world'
         data, addr = self.recieve()
         #messagebox.showinfo('Message', 'hello,{}'.format(name))
-        self.s.sendto(b'hello,%s' % data, addr)
-        info = 'send {} to address: {}'.format(name, addr)
+        name = name.encode('ascii')
+        self.s.sendto(b'%s' % name, addr)
+        rec = 'Recieve from %s:%s' % addr + '\t' + \
+            'info:{}'.format(data.decode('utf-8'))
+        self.listen = Label(self, text=rec)
+        self.listen.pack()
+        info = 'send {} to address: {}'.format(name.decode('utf-8'), addr)
         self.nameOutput = Label(self, text=info)
         self.nameOutput.pack()
 
     def set_param(self, port=9999):
         param = (self.__ip, port)
         self.s.bind(param)
-        self.flag = True
 
     def recieve(self):
         data, addr = self.s.recvfrom(self.__MAXLEN)
@@ -42,5 +46,6 @@ class Appliacation(Frame):
 
 
 app = Appliacation()
+app.set_param()
 app.master.title('hello,world!')
 app.mainloop()
